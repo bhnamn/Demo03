e68b2360bfefd84e3fd770aff199323539271cfe
bhnamn <borhan.amini@gmail.com>
2016-01-19 12:16:19 +0200

    initial version

---
+++ Teh2/WashingMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teh2
+{
+    class WashingMachine
+    {
+        public int WaterTemp { get; set; }
+        public int SpinSpeed { get; set; }
+        public string WashingMethod { get; set; }
+        public bool Prewash { get; set; }
+        public void PrintData()
+        {
+            Console.WriteLine("Washing machine prog.: ");
+            Console.WriteLine("-Water temp: "+WaterTemp);
+            Console.WriteLine("-Spin speed: " + SpinSpeed);
+            Console.WriteLine("-Washing method: " + WashingMethod);
+            Console.WriteLine("-Prewash: " + Prewash);
+
+        }
+    }
+}
